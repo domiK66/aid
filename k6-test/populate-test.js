b30ec80edd4e92571e513cf776c61f144d8d3083
domiK66 <73217569+domiK66@users.noreply.github.com>
2021-10-21 14:40:01 +0200

    Update populate-test.js

--- k6-test/populate-test.js
+++ k6-test/populate-test.js
@@ -2,14 +2,12 @@ import http from 'k6/http';
 
 import { sleep } from 'k6';
 
-
 export default function () {
-  
   var url = "http://20.103.17.59/guestbook.php?cmd=set&value=,chef,2";
-    for (let i = 0; i < 1000; i++) { 
-      let params = http.get("http://20.103.17.59/guestbook.php?cmd=get")
-      url += params + "," + i.toString()
-      http.get(url);
-      sleep(1);
-    }
+  for (let i = 0; i < 1000; i++) { 
+    let params = http.get("http://20.103.17.59/guestbook.php?cmd=get")
+    url += params + "," + i.toString()
+    http.get(url);
+    sleep(1);
+  }
 }
