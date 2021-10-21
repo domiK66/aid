43320954429597bcca5de13320146d98c0211573
domiK66 <73217569+domiK66@users.noreply.github.com>
2021-10-21 13:35:23 +0200

    Create stress-test.js

---
+++ k6-test/stress-test.js
@@ -0,0 +1,26 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '2m', target: 100 }, // below normal load
+    { duration: '5m', target: 100 },
+    { duration: '2m', target: 200 }, // normal load
+    { duration: '5m', target: 200 },
+    { duration: '2m', target: 300 }, // around the breaking point
+    { duration: '5m', target: 300 },
+    { duration: '2m', target: 400 }, // beyond the breaking point
+    { duration: '5m', target: 400 },
+    { duration: '10m', target: 0 }, // scale down. Recovery stage.
+  ],
+};
+
+export default function () {
+  const BASE_URL = 'http://20.103.17.59/'; 
+
+  const responses = http.batch([
+    ['GET', `${BASE_URL}/guestbook.php?cmd=get`, null]
+  ]);
+
+  sleep(1);
+}
