323fbc51f2c9f5e1cacc83b06f2e166ad48a219f
Dominik Kainz <dominik.kainz@edu.fh-joanneum.at>
2023-11-21 10:33:00 +0100

    class var

--- locust/main.py
+++ locust/main.py
@@ -5,11 +5,12 @@ default_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4
 class WebsiteUser(HttpUser):
     wait_time = between(1, 2)
     number = 0
+
     @task(1)
     def get_index(self):
         self.client.get("/guestbook.php?cmd=get", headers=default_headers)
 
     @task(2)
     def post(self):
-        self.client.post(f$"/guestbook.php?cmd=set&value=,Robert {number}", headers=default_headers)
-        number = number + 1
+        self.client.post(f$"/guestbook.php?cmd=set&value=,Robert {self.number}", headers=default_headers)
+        self.number = number + 1
