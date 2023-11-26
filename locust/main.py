27238a4a4dbbb9ac281c2f991d5865378a7c4f30
Dominik Kainz <dominik.kainz@edu.fh-joanneum.at>
2023-11-26 18:26:42 +0100

    add range

--- locust/main.py
+++ locust/main.py
@@ -3,14 +3,12 @@ from locust import HttpUser, task, between
 default_headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
 class WebsiteUser(HttpUser):
-    wait_time = between(1, 2)
-    number = 0
-
+    wait_time = between(1, 5)
     @task(1)
     def get_index(self):
         self.client.get("/guestbook.php?cmd=get", headers=default_headers)
 
     @task(2)
     def post(self):
-        self.client.post(f"/guestbook.php?cmd=set&value=,Robert {self.number}", headers=default_headers)
-        self.number += 1    
+        for i in range(100):
+            self.client.post("/guestbook.php?cmd=set&value=Test %i" % i, headers=default_headers, name="/guestbook.php?cmd=set&value=[value]")
